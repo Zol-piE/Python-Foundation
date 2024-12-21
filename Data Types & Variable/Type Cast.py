57672799d38318906e9ad5febcf20dec8ec821c5
Tohidul Islam <129326995+Zol-piE@users.noreply.github.com>
2024-12-22 02:16:23 +0600

    Create Type Cast.py

---
+++ Data Types & Variable/Type Cast.py
@@ -0,0 +1,18 @@
+x = 100
+y = "20"
+print(type(x),type(y))
+x = x + int(20)
+print(x)
+#type cast any data type to binary-hex-oct -> int(variable,base) -> base->2,8,16
+x= "1010"
+print(int(x,2))
+print(int(x,8))
+print(int(x,16))
+x = "13"
+print(int(x,16))
+x = 13
+print(bin(x))
+print(oct(x))
+print(hex(x))
+
+
