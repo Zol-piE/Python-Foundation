c2a72ca3e156ca74b661d8eed356b15d7a0256de
Tohidul Islam <129326995+Zol-piE@users.noreply.github.com>
2024-12-25 23:55:35 +0600

    Update tuples.py

--- Container/tuples.py
+++ Container/tuples.py
@@ -1,4 +1,5 @@
 #Tuples are "Read-Only" -> after creation can not add,remove,change elements
+
 num = (1,2,3,4,5)
 for i in range(len(num)):
     print(num[i],end=" ")
@@ -7,3 +8,15 @@ try:
     num[i] =10
 except TypeError:
     print("Item Change is not Possible")
+
+# () is optinal but "," is 
+
+num = 10,20,30
+print(type(num))
+num = (10) # count  as int
+print(type(num))
+#adding Two Tuples
+even = 2,4,6,8
+odd = 3,5,7,9
+num = even+odd
+print(num)
