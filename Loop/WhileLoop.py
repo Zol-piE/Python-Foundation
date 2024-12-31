eb55b9ebba0f8ad5caf740d4ec820b681ca8775c
Tohidul Islam <129326995+Zol-piE@users.noreply.github.com>
2024-12-31 14:59:22 +0600

    Create WhileLoop.py

---
+++ Loop/WhileLoop.py
@@ -0,0 +1,13 @@
+"""
+while condition:
+    statement....
+
+"""
+n = 5
+i =0
+while i<n:
+    print(i)
+    i+=1
+else:
+    print("Loop ended") #executes after the while loop condition is filled
+
