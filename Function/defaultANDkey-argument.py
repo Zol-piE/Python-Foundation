d0c9ac36d48cb255c3f43d3d17be99f43d84ebf8
Tohidul Islam <129326995+Zol-piE@users.noreply.github.com>
2024-12-31 14:59:05 +0600

    Create defaultANDkey-argument.py

---
+++ Function/defaultANDkey-argument.py
@@ -0,0 +1,10 @@
+def fun(role,name="NA",roll="NA"):
+    print(role,name,roll)
+
+def fun2(name,id,role):
+    print(name,id,role)
+
+#main Function
+fun("strundent")
+fun("tomal",17,"student")
+fun(name="Tomal",role="Teacher",roll=9)
